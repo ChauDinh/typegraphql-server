5066b09abdc6deefc7d4744e0f7513fa2466b8d0
Chau Dinh <chaudvb95uc@gmail.com>
2020-05-17 12:22:53 +0700

    user input validtion: validate user input with class-validator

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,16 +1,10 @@
-import {
-  Resolver,
-  Query,
-  Mutation,
-  Arg,
-  FieldResolver,
-  Root,
-} from "type-graphql";
-import * as bcrypt from "bcryptjs";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import bcrypt from "bcryptjs";
 
 import { User } from "./../../entity/User";
+import { RegisterInput } from "./register/RegisterInput";
 
-@Resolver(User)
+@Resolver()
 export class RegisterResolver {
   @Query(() => String, {
     nullable: true,
@@ -20,17 +14,9 @@ export class RegisterResolver {
     return "hello, world";
   }
 
-  @FieldResolver()
-  async name(@Root() parent: User) {
-    return `${parent.firstName} ${parent.lastName}`;
-  }
-
   @Mutation(() => User)
   async register(
-    @Arg("firstName") firstName: string,
-    @Arg("lastName") lastName: string,
-    @Arg("email") email: string,
-    @Arg("password") password: string
+    @Arg("data") { firstName, lastName, email, password }: RegisterInput
   ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
 
